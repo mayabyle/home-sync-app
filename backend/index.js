34fea71d15b774048f06b8204c5cd5f9dc8a2785
bylem <bylemaya@gmail.com>
2023-09-26 12:55:36 +0300

    chores page works

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,91 @@ import cors from "cors";
 
 const app = express()
 app.use(cors())
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Ambacyda1!",
-    database: "test"
+    database: "apartment"
 })
 
-db.connect((err) => {
-    if (err) {
-      console.error("Error connecting to the database:", err);
-    } else {
-      console.log("Connected to the database!");
-    }
-  });
+// db.connect((err) => {
+//     if (err) {
+//       console.error("Error connecting to the database:", err);
+//     } else {
+//       console.log("Connected to the database!");
+//     }
+// });
 
+
+app.get("/", (req,res) => {
+    res.json("hello im backend")
+})
+
+app.get("/chores", (req, res) => {
+    const selectQuery = "SELECT * FROM chores"
+    db.query(selectQuery, (err, data) => {
+        if(err) {
+            return res.json("error")
+        }
+        return res.json(data)
+    })
+})
+
+app.post("/chores", (req, res) => {
+    const values = [
+        req.body.desc,
+        req.body.incharge,
+        // "",  //TODO - to support roomies incharge
+    ];
+
+    const insertQuery = "INSERT INTO chores(`desc`, `incharge`) VALUES (?)";
+    db.query(insertQuery, [values], (err, insertRes) => {
+        if (err) {
+            return res.status(500).json({ error: "Error inserting data into the database." });
+        }
+
+        // Fetch and return the newly inserted data
+        const selectQuery = "SELECT * FROM chores WHERE id = ?"; 
+        db.query(selectQuery, [insertRes.insertId], (err, selectRes) => {
+            if (err) {
+                return res.status(500).json({ error: "Error fetching inserted data from the database." });
+            }
+            return res.status(201).json(selectRes[0]);
+        });
+    })
+})
+
+app.delete("/chores/:id", (req, res) => {
+    const choreId = req.params.id
+    const deleteQuery = "DELETE FROM chores WHERE id = ? ";
+
+    db.query(deleteQuery, [choreId], (err, data) => {
+        if (err) 
+            return res.send(err);
+        return res.status(200).send("Successfully deleted from chores");
+  });
+})
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+app.listen(3001, ()=> {
+    console.log("connectet to backend!")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const chores = [
     {
         id: 1,
@@ -37,39 +106,4 @@ const chores = [
         incharge: "Bar",
     },
 ];
-
-app.get("/", (req,res) => {
-    res.json("hello im backend")
-})
-
-app.get("/chores", (req,res) => {
-    return res.json(chores)
-})
-
-app.get("/books", (req,res) => {
-    const myquery = "SELECT * FROM books"
-    db.query(myquery, (err, data) => {
-        if(err) {
-            return res.json("error")
-        }
-        return res.json(data)
-    })
-})
-
-app.post("/books", (req,res) => {
-    const myquery = "INSERT INTO books ('title', 'desc', 'cover') VALUES (?)"
-    const val = ["tit", "desc", "pic"]
-    db.query(myquery, (err, data) => {
-        if(err) {
-            return res.json("error")
-        }
-        return res.json(data)
-    })
-})
-
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-app.listen(3000, ()=> {
-    console.log("connectet to backend!")
-})
+*/
--- client/src/pages/Chores.jsx
+++ client/src/pages/Chores.jsx
@@ -3,31 +3,38 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 
 function Chores() {
-    const [chore, setChore] = useState("");
+    const [desc, setDesc] = useState("");
     const [incharge, setIncharge] = useState("");  //TODO change into list or check box
     const [chores, setChores] = useState([]);
 
     useEffect(() => {
         const fetchChores = async () => {
             try {
-                const res = await axios.get("http://localhost:3000/chores")
-                console.log(res)
+                const res = await axios.get(`http://localhost:3001/chores`)
                 setChores(res.data)
             } 
             catch(err) { console.log(err) }
         }
         fetchChores()
-    })
+    },[])
 
-    const addChore = () => {
-        setChores([...chores, {chore, incharge}]);
-        setChore("");
+    const handleAdd = async () => {
+        try {
+            const res = await axios.post(`http://localhost:3001/chores`, {desc, incharge})
+            const newChore = res.data
+            setChores([...chores, { id: newChore.id, desc: newChore.desc, incharge: newChore.incharge }]);
+        } 
+        catch(e) { console.log(e) }
+        
+        setDesc("");
         setIncharge("");
     };
 
-    const deleteChore = (text) => {
+    const handleDelete = async (toDelete) => {
+        const res = await axios.delete(`http://localhost:3001/chores/${toDelete.id}`)
+        console.log(res.data)
         const newChores = chores.filter((chore) => {
-          return chore !== text;
+            return chore !== toDelete;
         });
         setChores(newChores);
       };
@@ -36,9 +43,9 @@ function Chores() {
         <div className="chores">
             <h4>chores:</h4>
             <form>
-                <input type="text" name="chore" value={chore} placeholder="Create a new chore" onChange={(e) => { setChore(e.target.value) }} />
-                {/* <input type="text" name="incharge" value={incharge} placeholder="incharge" onChange={(e) => { setIncharge(e.target.value) }} /> */}
-                <div className="incharge-box">
+                <input type="text" name="desc" value={desc} placeholder="Create a new chore" onChange={(e) => { setDesc(e.target.value) }} />
+                <input type="text" name="incharge" value={incharge} placeholder="incharge" onChange={(e) => { setIncharge(e.target.value) }} />
+                {/* <div className="incharge-box">
                     <label>
                         <input type="checkbox" name="roomates" value="bar" id="1" />
                         Bar
@@ -47,20 +54,18 @@ function Chores() {
                         <input type="checkbox" name="roomates" value="maya" id="2" />
                         Maya
                     </label>
-                </div>
+                </div> */}
             </form>
-            <button className="add-button" onClick={ addChore }>
-                Add
-            </button>
+            <button className="add-button" onClick={ handleAdd }> Add </button>
 
             {chores?.length > 0 ? (
                 <ul className="chore-list">
-                    {chores.map((chore, index) => (
-                        <div className="chore">
-                            <li key={index}> 
-                            <h5>{chore.incharge+" -  "+chore.chore}</h5>
+                    {chores.map((chore) => (
+                        <div className="chore" key={chore.id}>
+                            <li> 
+                                <h5>{chore.desc+" -  "+chore.incharge}</h5>
                             </li>
-                            <button className="delete-button" onClick={() => { deleteChore(chore) }} >
+                            <button className="delete-button" onClick={() => { handleDelete(chore) }} >
                                 Delete
                             </button>
                         </div>
